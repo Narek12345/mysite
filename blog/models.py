a4aaf7414929c2d22eea97e852b739493986a14d
Narek <narekbayanduryan16@gmail.com>
2023-09-28 22:08:49 +0300

    Adding a many-to-one relationship in the 'Post' model of a 'blog' app.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -12,6 +13,7 @@ class Post(models.Model):
 
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
 	body = models.TextField()
 	publish = models.DateTimeField(default=timezone.now)
 	created = models.DateTimeField(auto_now_add=True)
