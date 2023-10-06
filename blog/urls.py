13be96c0fe0f1e9f9a9929d714f2db400bebf11b
Narek <narekbayanduryan16@gmail.com>
2023-10-06 22:14:45 +0300

    Modifying URL templates.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
 	# Post views.
 	path('', views.post_list, name='post_list'),
-	path('<int:id>', views.post_detail, name='post_detail'),
+	path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
