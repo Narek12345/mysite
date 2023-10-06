8334948256339e3653d333a45074bfb581be8ced
Narek <narekbayanduryan16@gmail.com>
2023-10-06 21:47:04 +0300

    Adding a unique_for_date field to create unique slugs.

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
 
 
 	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250)
+	slug = models.SlugField(max_length=250, unique_for_date='publish')
 	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
 	body = models.TextField()
 	publish = models.DateTimeField(default=timezone.now)
