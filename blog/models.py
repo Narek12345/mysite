bbe3a232e35d83540313501fb318001d35f5a256
Narek <narekbayanduryan16@gmail.com>
2023-10-06 22:25:25 +0300

    Modifying the canonical URL of posts.

--- blog/models.py
+++ blog/models.py
@@ -43,4 +43,4 @@ class Post(models.Model):
 
 
 	def get_absolute_url(self):
-		return reverse('blog:post_detail', args=[self.id])
+		return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
