abc606fbf8d94d47295a06831e335a4858394ab1
Narek <narekbayanduryan16@gmail.com>
2023-10-07 18:37:54 +0300

    Adding comments to the post details view.

--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,11 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
-	return render(request, 'blog/post/detail.html', {'post': post})
+	# List of active comments for this post.
+	comments = post.comments.filter(active=True)
+	# User comment form.
+	form = CommentForm()
+	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
