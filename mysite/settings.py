388c71f9814e3257e5af3755074dd537f48d2a35
Narek <narekbayanduryan16@gmail.com>
2023-10-07 14:41:32 +0300

    Creating configurations for email sending.

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email server configuration.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
