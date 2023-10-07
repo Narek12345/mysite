54bf863a8da8bc7b22acd0e32ac980a5af995f13
Narek <narekbayanduryan16@gmail.com>
2023-10-07 13:46:02 +0300

    Creating view for send email.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -22,3 +23,18 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
 	return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+	# Extract post by id.
+	post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+	if request.method == 'POST':
+		# The form has been submitted for processing.
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			# The form fields were successfully validated.
+			cd = forms.cleaned_data
+			# Send an email.
+	else:
+		form = EmailPostForm()
+	return render(request, 'blog/post/shate.html', {'post': post, 'form': form})
