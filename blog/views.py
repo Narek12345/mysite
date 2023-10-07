cc5451ca43694b7150b25d0addafa87b33e09276
Narek <narekbayanduryan16@gmail.com>
2023-10-07 21:02:31 +0300

    Adding Post aggregation using Count.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.conf import settings
+from django.db.models import Count
 from taggit.models import Tag
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
@@ -32,9 +33,16 @@ def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
 	# List of active comments for this post.
 	comments = post.comments.filter(active=True)
+	
 	# User comment form.
 	form = CommentForm()
-	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+
+	# List of similat posts.
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similat_posts': similar_posts})
 
 
 def post_share(request, post_id):
