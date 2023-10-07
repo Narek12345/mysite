ef11b5cb866d29c5ec9076d7f526be1ceac6886b
Narek <narekbayanduryan16@gmail.com>
2023-10-07 17:43:56 +0300

    Created a view to process the comment.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.conf import settings
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 
 def post_list(request):
@@ -47,3 +48,19 @@ def post_share(request, post_id):
 	else:
 		form = EmailPostForm()
 	return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+	post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+	comment = None
+	# Comment sent.
+	form = CommentForm(data=request.POST)
+	if form.is_valid():
+		# Create a Comment class object without storing it in the db.
+		comment = form.save(commit=False)
+		# Assign post to comment.
+		comment.post = post
+		# Save comment in db.
+		comment.save()
+	return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
