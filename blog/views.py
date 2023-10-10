e7cb35d62d6a0bf70fccf7329d528f9ad4e39d11
Narek <narekbayanduryan16@gmail.com>
2023-10-10 21:11:45 +0300

    Add a relevance filter to the search view.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from taggit.models import Tag
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.mail import send_mail
 from django.conf import settings
 from django.db.models import Count
@@ -95,8 +95,11 @@ def post_search(request):
 		form = SearchForm(request.GET)
 		if form.is_valid():
 			query = form.cleaned_data['query']
+			search_vector = SearchVector('title', 'body')
+			search_query = SearchQuery(query)
 			results = Post.published.annotate(
-				search=SearchVector('title', 'body'),
-			).filter(search=query)
+				search=search_vector,
+				rank=SearchRank(search_vector, search_query)
+			).filter(search=search_query).order_by('-rank')
 
 	return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
