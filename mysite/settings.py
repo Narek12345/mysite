d52a919f33dfdedfe295efd58faa59fc0846df2d
Narek <narekbayanduryan16@gmail.com>
2023-10-10 17:43:14 +0300

    Adding the django.contrib.postgres app.

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 
     # My apps:
     'blog.apps.BlogConfig',
