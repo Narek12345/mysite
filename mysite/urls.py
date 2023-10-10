415b315209cc9c84f5f2e5095798e94920ffce5f
Narek <narekbayanduryan16@gmail.com>
2023-10-10 23:50:46 +0300

    A link to the list_apps app has been written.

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,5 +26,6 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('', include('list_apps.urls', namespace='list_apps')),
 ]
