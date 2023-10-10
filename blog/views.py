c5f8206755ded93b4a67f6f2cc1cf08d63cd0370
Narek <narekbayanduryan16@gmail.com>
2023-10-10 17:53:24 +0300

    Created a view to process the request.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
+from taggit.models import Tag
+
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.conf import settings
 from django.db.models import Count
-from taggit.models import Tag
-from .forms import EmailPostForm, CommentForm
+
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 
 
@@ -81,3 +84,19 @@ def post_comment(request, post_id):
 		# Save comment in db.
 		comment.save()
 	return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+	form = SearchForm()
+	query = None
+	results = []
+
+	if 'query' in request.GET:
+		form = SearchForm(request.GET)
+		if form.is_valid():
+			query = form.cleaned_data['query']
+			results = Post.published.annotate(
+				search=SearchVector('title', 'body'),
+			).filter(search=query)
+
+	return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
