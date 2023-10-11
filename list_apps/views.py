991361f025bc12a692105acdb68dd5dcc888f357
Narek <narekbayanduryan16@gmail.com>
2023-10-11 21:50:52 +0300

    Updating the index view in list_apps.

--- list_apps/views.py
+++ list_apps/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-	list_apps = ['http://127.0.0.1:8000/blog/']
-	return render(request, 'list_apps/index.html', {'list_apps': list_apps})
+	return render(request, 'list_apps/index.html')
